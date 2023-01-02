cfdb6fafd061f2adbabfda7f2deb59802724ea22
grillow <spmr757@gmail.com>
2023-01-02 06:24:52 +0300

    async/await

--- main.py
+++ main.py
@@ -23,31 +23,31 @@ async def on_ready():
     for guild in dd.guilds:
         if guild.id in discord_guild_ids:
             for channel in guild.voice_channels:
-                session_manager.create_session(channel)
+                await session_manager.create_session(channel)
 
 
 @dd.event
 async def on_user_update(before: discord.User, after: discord.User):
-    session_manager.user_updated(after)
+    await session_manager.user_updated(after)
 
 
 @dd.event
 async def on_voice_state_update(member: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
     if before.channel is None and after.channel is not None:
         if after.channel.guild.id in discord_guild_ids:
-            session_manager.member_connected(after.channel, member)
+            await session_manager.member_connected(after.channel, member)
     elif before.channel is not None and after.channel is None:
         if before.channel.guild.id in discord_guild_ids:
-            session_manager.member_disconnected(before.channel, member)
+            await session_manager.member_disconnected(before.channel, member)
     elif before.channel is not None and after.channel is not None:
         if before.channel != after.channel:
             if before.channel.guild.id in discord_guild_ids:
-                session_manager.member_disconnected(before.channel, member)
+                await session_manager.member_disconnected(before.channel, member)
             if after.channel.guild.id in discord_guild_ids:
-                session_manager.member_connected(after.channel, member)
+                await session_manager.member_connected(after.channel, member)
         else:
             if after.channel.guild.id in discord_guild_ids:
-                session_manager.member_voice_updated(after.channel, member)
+                await session_manager.member_voice_updated(after.channel, member)
 
 
 dd.run(discord_token)
--- model.py
+++ model.py
@@ -66,53 +66,71 @@ def build_message_string(server_name: str, channel_name: str, started: datetime,
 
 
 class TelegramMessage:
-    def __init__(self, api: telegram.Bot, chat_id: str, server_name: str, channel_name: str, started: datetime,
-                 connected_users=None):
+    def __init__(self, api: telegram.Bot, chat_id: str, message_id: int):
         self.api = api
         self.chat_id = chat_id
+        self.message_id = message_id
+
+    @classmethod
+    async def create(cls, api: telegram.Bot, chat_id: str, server_name: str, channel_name: str, started: datetime,
+                     connected_users=None):
         text = build_message_string(server_name=server_name, channel_name=channel_name, started=started,
                                     connected_users=connected_users)
-        self.message_id = self.api.sendMessage(chat_id=chat_id, text=text).message_id
+        telegram_message = await api.sendMessage(chat_id=chat_id, text=text)
+        message_id = telegram_message.message_id
+        return TelegramMessage(api, chat_id, message_id)
 
-    def set_content(self, connected_users, server_name: str, channel_name: str, started: datetime, ended=None):
+    async def set_content(self, connected_users, server_name: str, channel_name: str, started: datetime, ended=None):
         text = build_message_string(server_name=server_name, channel_name=channel_name, started=started,
                                     ended=ended, connected_users=connected_users)
-        self.api.edit_message_text(chat_id=self.chat_id, message_id=self.message_id, text=text)
+        await self.api.edit_message_text(chat_id=self.chat_id, message_id=self.message_id, text=text)
 
 
 class VoiceSession:
-    def __init__(self, server_name: str, channel_name: str, telegram_api, telegram_chat_id, connected_members=None):
+    def __init__(self, server_name: str, channel_name: str, started: datetime, ended: datetime,
+                 connected_members: [], telegram_message: TelegramMessage, dead: bool):
         self.server_name = server_name
         self.channel_name = channel_name
-        self.started = datetime.datetime.now()
-        self.ended = None
-        self.connected_members = {}
-        if connected_members is not None:
-            for u in connected_members:
-                self.connected_members[u.id] = u
-        self.telegram_message = TelegramMessage(api=telegram_api, chat_id=telegram_chat_id, started=self.started,
-                                                server_name=server_name,
-                                                channel_name=self.channel_name,
-                                                connected_users=self.connected_members.values())
-        self.dead = False
+        self.started = started
+        self.ended = ended
+        self.connected_members = connected_members
+        self.telegram_message = telegram_message
+        self.dead = dead
 
-    def user_connected(self, user_id: int, user: UserState):
+    @classmethod
+    async def create(cls, server_name: str, channel_name: str, telegram_api, telegram_chat_id,
+                     connected_members: [UserState]):
+        started = datetime.datetime.now()
+        ended = None
+        connected_members_map = {}
+        for u in connected_members:
+            connected_members_map[u.id] = u
+        telegram_message = await TelegramMessage.create(api=telegram_api, chat_id=telegram_chat_id,
+                                                        started=started,
+                                                        server_name=server_name,
+                                                        channel_name=channel_name,
+                                                        connected_users=connected_members_map.values())
+        dead = False
+        return VoiceSession(server_name, channel_name, started, ended,
+                            connected_members_map, telegram_message, dead)
+
+    async def user_connected(self, user_id: int, user: UserState):
         self.connected_members[user_id] = user
-        self.telegram_message.set_content(self.connected_members.values(), self.server_name, self.channel_name,
-                                          self.started, self.ended)
+        await self.telegram_message.set_content(self.connected_members.values(), self.server_name, self.channel_name,
+                                                self.started, self.ended)
 
-    def user_updated(self, user_id: int, user: UserState):
+    async def user_updated(self, user_id: int, user: UserState):
         self.connected_members[user_id] = user
-        self.telegram_message.set_content(self.connected_members.values(), self.server_name, self.channel_name,
-                                          self.started, self.ended)
+        await self.telegram_message.set_content(self.connected_members.values(), self.server_name, self.channel_name,
+                                                self.started, self.ended)
 
-    def user_disconnected(self, user_id: int):
+    async def user_disconnected(self, user_id: int):
         del self.connected_members[user_id]
         if len(self.connected_members) == 0:
             self.ended = datetime.datetime.now()
             self.dead = True
-        self.telegram_message.set_content(self.connected_members.values(), self.server_name, self.channel_name,
-                                          self.started, self.ended)
+        await self.telegram_message.set_content(self.connected_members.values(), self.server_name, self.channel_name,
+                                                self.started, self.ended)
 
 
 class SessionManager:
@@ -121,33 +139,33 @@ class SessionManager:
         self.telegram_api = telegram_api
         self.telegram_chat_id = telegram_chat_id
 
-    def create_session(self, channel):
+    async def create_session(self, channel):
         if len(channel.members) != 0:
-            self.sessions[channel.id] = VoiceSession(server_name=f'{channel.guild.name}',
-                                                     channel_name=f'{channel.name}',
-                                                     telegram_api=self.telegram_api,
-                                                     telegram_chat_id=self.telegram_chat_id,
-                                                     connected_members=list(
-                                                         map(discord_member_to_user, channel.members)))
+            self.sessions[channel.id] = await VoiceSession.create(server_name=f'{channel.guild.name}',
+                                                                  channel_name=f'{channel.name}',
+                                                                  telegram_api=self.telegram_api,
+                                                                  telegram_chat_id=self.telegram_chat_id,
+                                                                  connected_members=list(
+                                                                      map(discord_member_to_user, channel.members)))
 
-    def member_connected(self, channel: discord.VoiceChannel, member: discord.Member):
+    async def member_connected(self, channel: discord.VoiceChannel, member: discord.Member):
         if channel.id not in self.sessions:
-            self.create_session(channel)
+            await self.create_session(channel)
         else:
-            self.sessions[channel.id].user_connected(member.id, discord_member_to_user(member))
+            await self.sessions[channel.id].user_connected(member.id, discord_member_to_user(member))
 
-    def user_updated(self, user: discord.User):
-        for guild in user.mutual_guilds:
+    async def user_updated(self, user: discord.User):
+        for guild in await user.mutual_guilds():
             for member in guild.members:
                 if member.id == user.id:
-                    self.sessions[member.voice.channel.id].user_updated(member.id, discord_member_to_user(member))
+                    await self.sessions[member.voice.channel.id].user_updated(member.id, discord_member_to_user(member))
                     return
 
-    def member_voice_updated(self, channel: discord.VoiceChannel, member: discord.Member):
+    async def member_voice_updated(self, channel: discord.VoiceChannel, member: discord.Member):
         if channel.id in self.sessions:
-            self.sessions[channel.id].user_updated(member.id, discord_member_to_user(member))
+            await self.sessions[channel.id].user_updated(member.id, discord_member_to_user(member))
 
-    def member_disconnected(self, channel: discord.VoiceChannel, member: discord.Member):
-        self.sessions[channel.id].user_disconnected(member.id)
+    async def member_disconnected(self, channel: discord.VoiceChannel, member: discord.Member):
+        await self.sessions[channel.id].user_disconnected(member.id)
         if self.sessions[channel.id].dead:
             del self.sessions[channel.id]
